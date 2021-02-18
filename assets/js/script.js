fbe15c75a55a88ca9e1807914ff025235d6c883b
savbennett5 <76716614+savbennett8@users.noreply.github.com>
2021-02-17 18:42:36 -0700

    psuedo code for js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//first screen needs high score link, timer, h1 "Coding Quiz Challenge" 
+    //explanation, & start quiz button
+//when button is clicked, the timer starts and first question appears
+//buttons need to highlight when I hover over them
+//a faded notification indicates whether it was correct or incorrect
+    //after it moves on to the next question
+//if incorrect, subtract 10 seconds from timer
+//time remaining when code is complete is the score
+//score page prompts for user's initials and submit
+//then they are taken to the high scores page
+//given the option to go back to start page or clear high scores
