25c3e56c045c2579358ac7cb886c6d81a19e4f15
savbennett5 <76716614+savbennett8@users.noreply.github.com>
2021-02-20 19:59:18 -0700

    css started, html modified, js set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,117 @@
 //score page prompts for user's initials and submit
 //then they are taken to the high scores page
 //given the option to go back to start page or clear high scores
+
+let timer = 75;
+let interval = 0;
+let correct = 0;
+let index = 0;
+
+//Start Section
+let quizTitle = document.querySelector("#title");
+let rules = document.querySelector("#rules");
+let start = document.querySelector("#start");
+
+//quiz section
+let quiz = document.querySelector("#quiz");
+let question = document.querySelector("#questionText");
+
+//multiple choice
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
+
+//result section
+let result = document.querySelector("#result");
+let finalScore = document.querySelector("#points");
+let initials = document.querySelector("intials");
+
+//get 'h4' from quiz section
+let choice_que = document.querySelector(".choice_que");
+
+//store user's answer
+let userAns = undefined;
+
+let MCQS = [{
+    question: "HTML stands for:",
+    choice1: "Hyperlinks and Text Markup Language",
+    choice2: "Hyper Text Markup Language",
+    choice3: "Hyper Text Making Language",
+    choice4: "Hyper Text Mark Language",
+    answer: 1
+}, {
+    question: "What does CSS stand for?",
+    choice1: "Colorful StyleSheet",
+    choice2: "Creative Style Sheet",
+    choice3: "Cascading Style Sheet",
+    choice4: "Computer Style Sheet",
+    answer: 2
+}, {
+    question: "Which HTML tag is used to define an internal style sheet",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "svg",
+    answer: 1
+}, {
+    question: "Which is the correct CSS syntax?",
+    choice1: "body{color:black}",
+    choice2: "{body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 0
+}, {
+    question: "How do you insert a comment in a JS file?",
+    choice1: "<!-- This is a comment -->",
+    choice2: "//This is a comment//",
+    choice3: "//This is a comment",
+    choice4: "/*This is a comment*/",
+    answer: 2, 
+    answer: 3
+}];
+
+//when start quiz button is clicked
+start.addEventListener("click", function() {
+    //first question is presented
+    //quiz.style.display = "block";
+    loadData();
+    //countDown starts
+    interval = setInterval(countDown, 1000);
+});
+
+//timer for quiz
+let countDown = function() {
+    timer--;
+    time.innerText = timer;
+    console.log(timer);
+    if (timer === 0) {
+        //end quiz
+    } else {
+        //next question
+    }
+}
+//setInterval(countDown,1000);
+
+let loadData = function() {
+    question.innerText = MCQS[index].question;
+    option1.innerText = MCQS[index].choice1;
+    option2.innerText = MCQS[index].choice2;
+    option3.innerText = MCQS[index].choice3;
+    option4.innerText = MCQS[index].choice4;
+    
+    timer = 75;
+}
+
+choice_que.forEach((choices,choiceNo) =>{
+    choices.addEventListener("click", ()=>{
+        choices.classList.add("active");
+        //check answer
+        if(choiceNo === MCQS[index].answer) {
+            //display correct
+            //keep current timer
+        } else {
+            //subtract 10 seconds from timer
+        }
+    });
+}
