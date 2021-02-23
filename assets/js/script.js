d3e49cab6a28f7b1dffb1820216e9fc8b9a3291a
savbennett8 <email@example.com>
2021-02-22 20:54:37 -0700

    quiz functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,8 @@ let interval = 0;
 let correct = 0;
 let index = 0;
 
+let topSection = document.querySelector("#top-content")
+
 //Start Section
 let startSection = document.querySelector("#container");
 let timerEl = document.querySelector(".count");
@@ -25,6 +27,7 @@ let start = document.querySelector("#start");
 let quizSection = document.querySelector(".quiz-box");
 let quiz = document.querySelector("#quiz");
 let question = document.querySelector("#questionText");
+let footer = document.querySelector(".footer");
 
 //multiple choice
 let option1 = document.querySelector("#option1");
@@ -34,11 +37,11 @@ let option4 = document.querySelector("#option4");
 
 //result section
 let result = document.querySelector("#result");
-let finalScore = document.querySelector("#points");
+let finalScore = document.querySelector("#final-score");
 let initials = document.querySelector("intials");
 
 //get 'h4' from quiz section
-let choice_que = document.querySelector(".choice_que");
+let choice_que = document.querySelectorAll(".choice_que");
 
 //store user's answer
 let userAns = undefined;
@@ -84,20 +87,21 @@ let MCQS = [{
 //when start quiz button is clicked
 start.addEventListener("click", function() {
     //first question is presented
-    startSection.style.opacity = 0;
-    quizSection.style.opacity = 1;
+    startSection.style.display = "none";
+    quizSection.style.display = "block";
     
-    //loadData();
+    loadData();
     //countDown starts
     countDown();
 });
 
-function countDown() {
-    let timeLeft = 75;
+let timeLeft = 75;
+let timeInterval;
 
+function countDown() {
     //timer for quiz
-    let timeInterval = setInterval(function() {
-        if (timer === 0) {
+    timeInterval = setInterval(function() {
+        if (timeLeft <= 0) {
         //end quiz
         clearInterval(timeInterval);
         } else {
@@ -111,22 +115,49 @@ function countDown() {
 
 
 let loadData = function() {
-    question.innerText = MCQS[index].question;
-    option1.innerText = MCQS[index].choice1;
-    option2.innerText = MCQS[index].choice2;
-    option3.innerText = MCQS[index].choice3;
-    option4.innerText = MCQS[index].choice4;
+    if (index < MCQS.length) {
+        question.innerText = MCQS[index].question;
+        option1.innerText = MCQS[index].choice1;
+        option2.innerText = MCQS[index].choice2;
+        option3.innerText = MCQS[index].choice3;
+        option4.innerText = MCQS[index].choice4;
+    } else {
+        endQuiz();
+    }
 }
 
+
+
 choice_que.forEach((choices, choiceNo) =>{
+    
     choices.addEventListener("click", ()=>{
+        console.dir(choices);
         choices.classList.add("active");
         //check answer
         if(choiceNo === MCQS[index].answer) {
             //display correct
-            //keep current timer
+            footer.style.display = "block";
+            footer.textContent = "Correct!";
+            console.log("correct");
         } else {
+            //display incorrect
+            footer.textContent = "incorrect";
+            console.log("incorrect");
             //subtract 10 seconds from timer
+            timeLeft = timeLeft - 10;
         }
+        //load new question
+        index++;
+        loadData();
     });
 })
+
+let endQuiz = function() {
+    result.style.display = "block";
+    quizSection.style.display = "none";
+    topSection.style.display = "none";
+
+    clearInterval(timeInterval);
+    finalScore.innerText = timeLeft;
+}
+
